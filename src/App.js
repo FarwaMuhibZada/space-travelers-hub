765487ad4a82e9f58b09b7e96fe4fc98a9729064
farwaMuhibzada <farwafarid2017@gmail.com>
2024-09-02 22:51:21 +0430

    added the Footer components .

--- src/App.js
+++ src/App.js
@@ -6,17 +6,19 @@ import Rockets from './pages/Rockets';
 import Missions from './pages/Missions';
 import MyProfile from './pages/MyProfile';
 import Dragons from './pages/Dragons';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Rockets />} />
-          <Route path="/missions" element={<Missions />} />
-          <Route path="/dragons" element={<Dragons />} />
-          <Route path="/my_profile" element={<MyProfile />} />
-        </Routes>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/dragons" element={<Dragons />} />
+        <Route path="/my_profile" element={<MyProfile />} />
+      </Routes>
+      <Footer />
     </Router>
   );
 }
